5d3c0eb34b2f5f70d059ec4e343a7734a61a2929
Brett Terpstra <me@brettterpstra.com>
2021-04-29 07:32:53 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -o /usr/local/bin/howzit2 'https://github.com/ttscoff/howzit/blob/master/howzit'
+chmod a+x /usr/local/bin/howzit2
+echo 'Installation complete, run `howzit` to test.'
