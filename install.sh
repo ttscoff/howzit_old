e3d4102db290134195da29b5f45876e61b39c316
Brett Terpstra <me@brettterpstra.com>
2021-05-16 11:25:11 -0500

    Fix for open command logging

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-HOWZIT='/usr/local/bin/howzit'
-curl -SsL -o $HOWZIT 'https://raw.githubusercontent.com/ttscoff/howzit/main/howzit'
-chmod a+x $HOWZIT
-echo "Installation complete, run $(basename $HOWZIT) to test."
+echo 'Please enter a path to a directory in your $PATH'
+read -p 'Where should howzit be installed? ' INSTALL_PATH
+
+INSTALL_PATH=${INSTALL_PATH/#\~/$HOME}
+
+if [[ -d "$INSTALL_PATH" ]]; then
+	HOWZIT=${INSTALL_PATH%/}/howzit
+else
+	echo "Invalid path: ${INSTALL_PATH}"
+	exit 1
+fi
+
+curl -SsL -o "$HOWZIT" 'https://raw.githubusercontent.com/ttscoff/howzit/main/howzit'
+chmod a+x "$HOWZIT"
+which howzit &>/dev/null
+if [[ $? == 0 ]]; then
+	echo "Installed to ${HOWZIT}, run $(basename "$HOWZIT") to test."
+else
+	echo "Installed to ${HOWZIT}, but it doesn't seem to be in your path. Ensure that \"$INSTALL_PATH\" is added to your shell's path environment."
+fi
